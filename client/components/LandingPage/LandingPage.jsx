428307d6894963b42f655b18a3f82cbd29ab02b6
Warren <warren.p.kwong@gmail.com>
2018-08-28 21:35:41 -0700

    Added hover actions on LandingPage.jsx elements

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -3,41 +3,100 @@ import React, { Component } from "react";
 import "./LandingPage.css";
 
 export default class Main extends Component {
+  constructor() {
+    super();
+    this.onHoverHandler = this.onHoverHandler.bind(this);
+    this.state = {
+      icons: ["developer", "photographer", "music enthusiast"],
+      hoverDeveloper: false,
+      hoverPhotographer: false,
+      hoverMusic: false
+    };
+  }
+
+  onHoverHandler(text) {
+    this.setState({ [text]: !this.state[text] });
+  }
+
   render() {
     return (
       <div className="landing-page">
         <div>
           <div>
-            <h1 className="header">
-              W <span className="divider">|</span> K
-              {/* W A R R E N <span className="divider">|</span> K W O N G */}
-            </h1>
+            <div className="header">
+              <span className="header-left-letter">W</span>
+              <span className="divider">|</span>
+              <span className="header-right-letter">K</span>
+            </div>
           </div>
-          {/*
           <div className="subheader">
-            <div className="icon">
-              <img
-                src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/code.png"
-                width="30"
-              />
-              <span>Developer</span>
+            <div
+              onMouseEnter={() => this.onHoverHandler("hoverDeveloper")}
+              onMouseLeave={() => this.onHoverHandler("hoverDeveloper")}
+            >
+              {this.state.hoverDeveloper && (
+                <div className="icon icon-hover">
+                  <img
+                    src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/code-orange.png"
+                    width="40"
+                  />
+                  <span>developer</span>
+                </div>
+              )}
+              {!this.state.hoverDeveloper && (
+                <div className="icon">
+                  <img
+                    src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/code.png"
+                    width="40"
+                  />
+                </div>
+              )}
             </div>
-            <div className="icon">
-              <img
-                src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/photo-camera.png"
-                width="30"
-              />
-              <span>Photographer</span>
+            <div
+              onMouseEnter={() => this.onHoverHandler("hoverPhotographer")}
+              onMouseLeave={() => this.onHoverHandler("hoverPhotographer")}
+            >
+              {this.state.hoverPhotographer && (
+                <div className="icon icon-hover">
+                  <img
+                    src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/photo-camera-orange.png"
+                    width="40"
+                  />
+                  <span>photographer</span>
+                </div>
+              )}
+              {!this.state.hoverPhotographer && (
+                <div className="icon">
+                  <img
+                    src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/photo-camera.png"
+                    width="40"
+                  />
+                </div>
+              )}
             </div>
-            <div className="icon">
-              <img
-                src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/headphones.png"
-                width="30"
-              />
-              <span>Music Enthusiast</span>
+            <div
+              onMouseEnter={() => this.onHoverHandler("hoverMusic")}
+              onMouseLeave={() => this.onHoverHandler("hoverMusic")}
+            >
+              {this.state.hoverMusic && (
+                <div className="icon icon-hover">
+                  <img
+                    src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/headphones-orange.png"
+                    width="40"
+                  />
+                  <span>music enthusiast</span>
+                </div>
+              )}
+              {!this.state.hoverMusic && (
+                <div className="icon">
+                  <img
+                    src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/headphones.png"
+                    width="40"
+                  />
+                </div>
+              )}
             </div>
           </div>
-           */}
         </div>
       </div>
     );
---
+++ client/components/NavBar/NavBar.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+//  css
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <div />
+      </div>
+    );
+  }
+}
