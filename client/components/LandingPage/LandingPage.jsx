f040a7a968a614a406b2f7b1503953a3e46ef266
Warren <warren.p.kwong@gmail.com>
2018-08-28 17:32:26 -0700

    reconfigured icon and background assets

--- client/components/LandingPage/LandingPage.jsx
+++ client/components/LandingPage/LandingPage.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 import "./LandingPage.css";
 
-import codeIcon from "./assets/code.png";
-import cameraIcon from "./assets/photo-camera.png";
-import headphonesIcon from "./assets/headphones.png";
-
 export default class Main extends Component {
   render() {
     return (
@@ -14,15 +10,24 @@ export default class Main extends Component {
           <h1>WARREN KWONG</h1>
           <div className="subheader">
             <div className="icon">
-              <img src={codeIcon} width="30" />
+              <img
+                src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/code.png"
+                width="30"
+              />
               <span>Developer</span>
             </div>
             <div className="icon">
-              <img src={cameraIcon} width="30" />
+              <img
+                src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/photo-camera.png"
+                width="30"
+              />
               <span>Photographer</span>
             </div>
             <div className="icon">
-              <img src={headphonesIcon} width="30" />
+              <img
+                src="https://s3-us-west-1.amazonaws.com/warrenkwong.com-assets/headphones.png"
+                width="30"
+              />
               <span>Music Enthusiast</span>
             </div>
           </div>
